59fcab8212d7d36ae8ec68d958f981863c0f41cc
JadenCooper <jaden_cooper@outlook.com>
2023-06-23 12:16:29 +1200

    Moved Bug Fix Over

--- GameDevGameJam/Assets/Scripts/Enemy/EnemyMelee.cs
+++ GameDevGameJam/Assets/Scripts/Enemy/EnemyMelee.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyMelee : MonoBehaviour
 {
     public KnockBack knockback;
-
+    public float attackDistance = 2;
     public void Attack()
     {
         if(gameObject.GetComponent<CharacterStats>() == null)
@@ -16,19 +16,22 @@ public class EnemyMelee : MonoBehaviour
         float damage = gameObject.GetComponent<CharacterStats>().damage.GetValue();
 
         GameObject player = GameObject.FindGameObjectWithTag("Player");
-
-        if (player == null)
+        float distance = Vector2.Distance(gameObject.transform.position, player.transform.position);
+        if (distance <= attackDistance && distance >= -attackDistance)
         {
-            Debug.Log(gameObject.name + " does not have the character script on it");
-            return;
-        }
+            if (player == null)
+            {
+                Debug.Log(gameObject.name + " does not have the character script on it");
+                return;
+            }
 
-        Vector2 playerPos = player.gameObject.transform.position;
-        knockback.Knock(playerPos, player);
+            Vector2 playerPos = player.gameObject.transform.position;
+            knockback.Knock(playerPos, player);
 
-        if(player.GetComponent<CharacterStats>() != null)
-        {
-            player.GetComponent<CharacterStats>().TakeDamage(damage);
+            if (player.GetComponent<CharacterStats>() != null)
+            {
+                player.GetComponent<CharacterStats>().TakeDamage(damage);
+            }
         }
     }
 }
--- GameDevGameJam/Assets/Scripts/Enemy/Zombie.cs
+++ GameDevGameJam/Assets/Scripts/Enemy/Zombie.cs
@@ -5,43 +5,11 @@ using UnityEngine;
 public class Zombie : MonoBehaviour
 {
     private AudioSource audioSource;
-    public float attackDistance = 2;
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
         StartCoroutine(MoanTimer(Random.Range(0.1f, 20f)));
     }
-
-    public void Attack()
-    {
-        if(gameObject.GetComponent<CharacterStats>() == null)
-        {
-            Debug.Log(gameObject.name + " does not have the character script on it");
-        }
-
-        float damage = gameObject.GetComponent<CharacterStats>().damage.GetValue();
-
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-        float distance = Vector2.Distance(gameObject.transform.position, player.transform.position);
-        Debug.Log(distance);
-        if (distance <= attackDistance && distance >= -attackDistance)
-        {
-            if (player == null)
-            {
-                Debug.Log(gameObject.name + " does not have the character script on it");
-                return;
-            }
-
-            Vector2 playerPos = player.gameObject.transform.position;
-            knockback.Knock(playerPos, player);
-
-            if (player.GetComponent<CharacterStats>() != null)
-            {
-                player.GetComponent<CharacterStats>().TakeDamage(damage);
-            }
-        }
-    }
-
     IEnumerator MoanTimer(float Timer)
     {
         yield return new WaitForSeconds(Timer);
